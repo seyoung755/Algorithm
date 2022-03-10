30e964ea555f63fe5f5d81619acbbd60d860f7ec
leezzangmin <64303390+leezzangmin@users.noreply.github.com>
2022-03-10 22:39:45 +0900

    Create 2003.py

---
+++ src/짱민/2003.py
@@ -0,0 +1,22 @@
+import sys;input=sys.stdin.readline
+N,M=map(int,input().split())
+A=list(map(int,input().split()))
+left,right=0,0
+
+s=A[0]
+ans=0
+while right<N:
+    if s<M:
+        right+=1
+        if right!=N:
+            s+=A[right]
+    elif s==M:
+        ans+=1
+        s-=A[left]
+        left+=1
+
+    else: #s>M
+        s-=A[left]
+        left+=1
+
+print(ans)
