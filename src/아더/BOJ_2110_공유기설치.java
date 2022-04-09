17fe01cc83febec316474a4a36c092a216f26d0f
ak2j38 <k2j38@naver.com>
2022-04-09 15:50:48 +0900

    add : BOJ 2110 문제 풀이
    1. 공유기 설치 문제 풀이 추가
    
    end

---
+++ src/아더/BOJ_2110_공유기설치.java
@@ -0,0 +1,64 @@
+package 아더;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class BOJ_2110_공유기설치 {
+    static int N, C;
+    static int[] houses;
+
+    public static void main(String[] args) {
+        try {
+            input();
+            solve();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void solve() {
+        Arrays.sort(houses, 1, N + 1);
+
+        int L = 1, R = 1_000_000_000, answer = 0;
+
+        while (L <= R) {
+            int mid = (L + R) / 2;
+            if (determination(mid)) {
+                answer = mid;
+                L = mid + 1;
+            } else {
+                R = mid - 1;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    private static boolean determination(int mid) {
+        int wifiCnt = 1;
+        int last = houses[1];
+
+        for (int i = 2; i <= N; i++) {
+            if (houses[i] - last < mid) continue;
+            wifiCnt++;
+            last = houses[i];
+        }
+
+        return wifiCnt >= C;
+    }
+
+    private static void input() throws IOException {
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new FileReader(new File("/Users/woo-jinpark/Desktop/Park/05_Test/input/input.txt")));
+
+        String[] split = br.readLine().split(" ");
+        N = Integer.parseInt(split[0]);
+        C = Integer.parseInt(split[1]);
+
+        houses = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            houses[i] = Integer.parseInt(br.readLine());
+        }
+    }
+}
